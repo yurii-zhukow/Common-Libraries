bc937b85f2ad769e5744e1f5d892ed0bfb6f73e7
YZ <yurii.zhukow@gmail.com>
2021-02-25 23:44:28 +0300

    add field attributes

--- YZ.Helpers/Helpers.Attributes.cs
+++ YZ.Helpers/Helpers.Attributes.cs
@@ -29,7 +29,34 @@ namespace YZ {
             if (value == null) return dflt?.Invoke(default);
             return (TAttr)Attribute.GetCustomAttribute(value.GetType(), typeof(TAttr), inherit) ?? dflt?.Invoke(value);
         }
+
+        public static TAttr[] GetAttrs<T, TAttr>(this T value, bool inherit) where TAttr : Attribute {
+            if (value == null) return new TAttr[0];
+            return Attribute.GetCustomAttributes(value.GetType(), typeof(TAttr), inherit)?.OfType<TAttr>().ToArray() ?? new TAttr[0];
+        }
+
+
+        public static TAttr GetAttr<T, TAttr>(this T value, string propName, bool inherit, Func<T, string, TAttr> dflt = null) where TAttr : Attribute {
+            if (value == null) return dflt?.Invoke(default, propName);
+            var fi = typeof(T).GetField(propName);
+            if (fi == null) return dflt?.Invoke(value, propName);
+            return fi.GetCustomAttribute<TAttr>(inherit) ?? dflt?.Invoke(value, propName);
+        }
+
+        public static TAttr[] GetAttrs<T, TAttr>(this T value, string propName, bool inherit) where TAttr : Attribute {
+            if (value == null) return new TAttr[0];
+            var fi = typeof(T).GetField(propName);
+            if (fi == null) return new TAttr[0];
+            return fi.GetCustomAttributes<TAttr>(inherit).OfType<TAttr>().ToArray() ?? new TAttr[0];
+        }
+
+
+        public static TValue[] GetAttrs<T, TAttr, TValue>(this T value, bool inherit, Func<TAttr, TValue> getValue) where TAttr : Attribute => GetAttrs<T, TAttr>(value, inherit).Select(getValue).ToArray();
+        public static TValue[] GetAttrs<T, TField, TAttr, TValue>(this T value, string propName, bool inherit, Func<TAttr, TValue> getValue) where TAttr : Attribute => GetAttrs<T, TAttr>(value, propName, inherit).Select(getValue).ToArray();
+
         public static TValue GetAttr<T, TAttr, TValue>(this T value, bool inherit, Func<TAttr, TValue> getValue, Func<T, TAttr> dflt = null) where TAttr : Attribute => getValue(GetAttr(value, inherit, dflt));
+        public static TValue GetAttr<T, TField, TAttr, TValue>(this T value, string propName, bool inherit, Func<TAttr, TValue> getValue, Func<T, string, TAttr> dflt = null) where TAttr : Attribute => getValue(GetAttr(value, propName, inherit, dflt));
+
 
         public static string GetDescription<TType>(this TType value, bool inherit = false) => GetAttr<TType, DescriptionAttribute>(value, inherit)?.Description ?? "";
         public static string GetBriefDescription<TType>(this TType value, bool inherit = false) => GetAttr<TType, BriefDescriptionAttribute>(value, inherit)?.BriefDescription ?? value.GetDescription(inherit);
@@ -43,6 +70,21 @@ namespace YZ {
             }
             return GetDescription(value, inherit);
         }
+
+        public static string GetDescription<TType>(this TType value, string propName, bool inherit = false) => GetAttr<TType, DescriptionAttribute>(value, inherit)?.Description ?? "";
+        public static string GetBriefDescription<TType>(this TType value, string propName, bool inherit = false) => GetAttr<TType, BriefDescriptionAttribute>(value, inherit)?.BriefDescription ?? value.GetDescription(inherit);
+
+        public static string GetDescription<TType>(this TType value, string propName, GetDescriptionMode mode, bool inherit = false) {
+            var brief = mode.HasFlag(GetDescriptionMode.Brief);
+
+            if (brief) {
+                var res = GetBriefDescription(value, propName, inherit);
+                return mode.HasFlag(GetDescriptionMode.Full) && string.IsNullOrEmpty(res) ? GetDescription(value, propName, GetDescriptionMode.Full, inherit) : res;
+            }
+            return GetDescription(value, propName, inherit);
+        }
+
+
 
     }
 }
