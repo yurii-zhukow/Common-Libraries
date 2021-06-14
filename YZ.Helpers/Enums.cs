cadb3954c9b1ee3f53bfdbdc56ff8d51000b910c
YZ <yurii.zhukow@gmail.com>
2021-06-14 12:01:05 +0300

    add enums

--- YZ.Helpers/Enums.cs
+++ YZ.Helpers/Enums.cs
@@ -7,6 +7,64 @@ using YZ;
 
 namespace YZ {
 
+
+    public enum Severity {
+        [Description("Не ошибка"), BriefDescription("Нет")] NotError = 0,
+        [Description("Замечание"), BriefDescription("Замеч.")] Hint = 1,
+        [Description("Предупреждение"), BriefDescription("Предупр.")] Warning = 2,
+        [Description("Ошибка"), BriefDescription("Ошибка")] Error = 3,
+        [Description("Критическая ошибка"), BriefDescription("Крит.ош.")] Critical = 4,
+        [Description("Фатальная ошибка"), BriefDescription("Фат.ош.")] Fatal = 5
+    }
+
+    public enum RussianCase {
+        /// <summary>
+        /// Именительный падеж
+        /// </summary>
+        [Description("Именительный")] Nominative = 1,
+
+        /// <summary>
+        /// Родительный падеж
+        /// </summary>
+        [Description("Родительный")] Genitive,
+        /// <summary>
+        /// Дательный падеж
+        /// </summary>
+        [Description("Дательный")] Dative,
+        /// <summary>
+        /// Винительный падеж
+        /// </summary>
+        [Description("Винительный")] Accusative,
+        /// <summary>
+        /// Творительный падеж
+        /// </summary>
+        [Description("Творительный")] Instrumental,
+        /// <summary>
+        /// Предложный падеж
+        /// </summary>
+        [Description("Предложный")] Prepositional
+    }
+
+    public enum RussianCount {
+        /// <summary>
+        /// Ноль, Пять, Шесть или много...
+        /// </summary>
+        Zero,
+        /// <summary>
+        /// Один, Двадцать Один... 
+        /// </summary>
+        One,
+        /// <summary>
+        /// Два, Три, Четыре...
+        /// </summary>
+        Two
+    }
+
+
+    public enum TimeUnit { Second, Minute, Hour, Day, Week, Month, Year }
+
+
+
     public enum LogPrefix {
         [Description(""), ConsoleColor(ConsoleColor.White)]
         None,
