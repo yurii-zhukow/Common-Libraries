2fadeffa54919fc76111ed2125d7e59b2b64c70c
YZ <yurii.zhukow@gmail.com>
2022-01-30 03:45:41 +0300

    converters

--- YZ.Helpers/Helpers.Converters.cs
+++ YZ.Helpers/Helpers.Converters.cs
@@ -8,7 +8,8 @@ namespace YZ {
 
 
     public static partial class Helpers {
-      
+        public static (T1 Item1, T2 Item2) Explode<T, T1, T2>(this T src, Func<T, T1> f1, Func<T, T2> f2) => (f1(src), f2(src));
+        public static Func<T, bool> Not<T>(Func<T, bool> fn) => (T src) => !fn(src);
     }
 
 }
