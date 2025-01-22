2c858e1df7826e3659b59997cc9fb91da397977e
Yurii Zhukow <yurii.zhukow@gmail.com>
2025-01-23 02:08:40 +0300

    Removed links to vulnerable package

--- YZ.Helpers/Helpers.Eval.cs
+++ YZ.Helpers/Helpers.Eval.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Dynamic.Core;
-using System.Linq.Dynamic.Core.Exceptions;
-using System.Linq.Dynamic.Core.Parser;
+//using System.Linq.Dynamic.Core;
+//using System.Linq.Dynamic.Core.Exceptions;
+//using System.Linq.Dynamic.Core.Parser;
 using System.Linq.Expressions;
 //using Microsoft.CodeAnalysis.CSharp.Scripting;
 //using Microsoft.CodeAnalysis.Scripting;
@@ -13,21 +13,21 @@ namespace YZ {
 
         //public static Script<T> Eval<T>(this string s, ScriptOptions options, Type globalsType) => CSharpScript.Create<T>(s, options, globalsType);
 
-        public static TResult Eval<TResult>(this string expr, IEnumerable<(string Name, object Value)> vars) {
+        //public static TResult Eval<TResult>(this string expr, IEnumerable<(string Name, object Value)> vars) {
 
-            vars = vars.OrderBy(t => t.Name).Distinct(new GenericEqualityComparer<(string Name, object Value), string>(t => t.Name)).ToArray();
-            var prms = vars.Select(t => Expression.Parameter(t.Value.GetType(), t.Name)).ToArray();
-            var vals = vars.Select(t => t.Value).ToArray();
+        //    vars = vars.OrderBy(t => t.Name).Distinct(new GenericEqualityComparer<(string Name, object Value), string>(t => t.Name)).ToArray();
+        //    var prms = vars.Select(t => Expression.Parameter(t.Value.GetType(), t.Name)).ToArray();
+        //    var vals = vars.Select(t => t.Value).ToArray();
 
-            Expression body = new ExpressionParser(prms, expr, prms, new ParsingConfig()).Parse(typeof(TResult));
+        //    Expression body = new ExpressionParser(prms, expr, prms, new ParsingConfig()).Parse(typeof(TResult));
 
-            LambdaExpression fn = Expression.Lambda(body, prms);
+        //    LambdaExpression fn = Expression.Lambda(body, prms);
 
-            //var fn = System.Linq.Dynamic.DynamicExpression.ParseLambda(prms, typeof(TResult), expr);
-            var res = fn.Compile().DynamicInvoke(vals);
-            if (res is TResult r) return r;
-            throw new ParseException($"Failed to parse expression {expr} with args [{vars.ToString(",", nv => $"{nv.Value.GetType().Name} {nv.Name} = {nv.Value.ToString().Ellipsis(100)}")}]", 0);
-        }
+        //    //var fn = System.Linq.Dynamic.DynamicExpression.ParseLambda(prms, typeof(TResult), expr);
+        //    var res = fn.Compile().DynamicInvoke(vals);
+        //    if (res is TResult r) return r;
+        //    throw new ParseException($"Failed to parse expression {expr} with args [{vars.ToString(",", nv => $"{nv.Value.GetType().Name} {nv.Name} = {nv.Value.ToString().Ellipsis(100)}")}]", 0);
+        //}
     }
 
 }
